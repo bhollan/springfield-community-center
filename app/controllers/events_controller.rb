eb7ee70750e4f94307705fdce258cab321326bbb
Brian Holland <brian.holland@proatria.com>
2016-04-30 15:28:21 -0500

    status messages in process

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,14 +8,20 @@ class EventsController < ApplicationController
       @event = Event.create(event_params)
       @event.update(user: current_user)
       @event.update(event_date: Date.strptime(params[:event][:event_date], '%m/%d/%Y'))
-      redirect_to event_path(@event)
+      redirect_to event_path(@event), alert: "New event created."
     else
-      redirect_to root_path
+      redirect_to new_user_session_path, alert: "You must be signed in to create events."
     end
   end
 
   def show
     @event = Event.find_by(id: params[:id])
+
+    if user_signed_in? && current_user.id == @event.user.id && @event.event_date >= Date.current
+      @modifiable_or_deletable = true
+    else
+      @modifiable_or_deletable = false
+    end
   end
 
   def index
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ActiveRecord::Base
   has_many :event_assets
   has_many :assets, through: :event_assets
   belongs_to :user
+  validate :public_events_must_register_early
+
+  def public_events_must_register_early
+    binding.pry
+  end
 end
